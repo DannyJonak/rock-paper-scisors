b29914931ecb4f2b3889cef48efee0b5f42d82ee
Danny Jonak <dannyjonak@gmail.com>
2022-09-15 17:37:56 -0400

    implement getComputerChoice function

--- rps_logic.js
+++ rps_logic.js
@@ -1,1 +1,15 @@
-console.log("Hello!")
+function getComputerChoice() {
+
+    //get a random number between 0 and 1 to use as a guess
+    let guessNum = Math.random();
+
+    //convert the random number guess into one of rock, paper, or scissors depending on its size
+    if (guessNum < 1/3) {
+        return "rock";
+    } else if (guessNum >= 1/3 && guessNum < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
