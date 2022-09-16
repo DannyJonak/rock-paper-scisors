46c393ccb9326e268852d308bf5be9c54f963878
Danny Jonak <dannyjonak@gmail.com>
2022-09-16 16:58:52 -0400

    implement function to play a single round of rock paper scissors

--- rps_logic.js
+++ rps_logic.js
@@ -5,11 +5,29 @@ function getComputerChoice() {
 
     //convert the random number guess into one of rock, paper, or scissors depending on its size
     if (guessNum < 1/3) {
-        return "rock";
+        return "Rock";
     } else if (guessNum >= 1/3 && guessNum < 2/3) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    //returns winner of playerSelection and computerSelection in a game of rock paper scissors
+
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper.";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Paper beats Scissors."
+    } else if (playerSelection < computerSelection) {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
